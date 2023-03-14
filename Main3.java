24b6cb77bd96215f8e2c7a0529ee304efca6b8ce
K.M.Swetha <94228215+swethamohanraj@users.noreply.github.com>
2023-03-14 08:55:28 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main3
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println("Area of rhombus:"+((num1*num2)/2));
+    }
+}
