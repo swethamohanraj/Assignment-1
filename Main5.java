3c9c8b4dbd22dcf1e6ccd9429865bb2f3b189542
K.M.Swetha <94228215+swethamohanraj@users.noreply.github.com>
2023-03-14 08:57:26 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,12 @@
+public class Main5
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=0;i<=20;i++)
+        {
+            System.out.print(+i+"\n");
+            i+= 1;
+        }
+    }
+}
