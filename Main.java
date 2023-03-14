a04fda61e0a9eb6fcbff17cc2634602f7723cb17
K.M.Swetha <94228215+swethamohanraj@users.noreply.github.com>
2023-03-14 08:50:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner num = new Scanner(System.in);
+        int num1 = num.nextInt();
+        int num2 = num.nextInt();
+        System.out.println("sum:"+(num1+num2));
+        System.out.println("mul:"+(num1*num2));
+        System.out.println("sub:"+(num1-num2));
+        System.out.println("div:"+(num1/num2));
+        System.out.println("rem:"+(num1%num2));
+    }
+}
